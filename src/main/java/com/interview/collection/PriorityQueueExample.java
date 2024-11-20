b7b49d66af30e24aa433a0fa21d43e622e448910
mdashikalikhan <khan.ashik@gmail.com>
2024-11-20 16:50:40 +0600

    Collection

---
+++ src/main/java/com/interview/collection/PriorityQueueExample.java
@@ -0,0 +1,14 @@
+package com.interview.collection;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        //pq.offer(null);
+        //pq.add(null);
+
+        System.out.println(pq.size());
+    }
+}
---
+++ src/main/java/com/interview/collection/TreesetTest.java
@@ -0,0 +1,12 @@
+package com.interview.collection;
+
+import java.util.TreeSet;
+
+public class TreesetTest {
+    public static void main(String[] args) {
+        TreeSet<String> set = new TreeSet<>();
+        set.add(null);
+
+
+    }
+}
