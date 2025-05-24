14afe8e9d102514821f5353ee70bd37298865b66
mdashikalikhan <khan.ashik@gmail.com>
2025-05-24 15:49:35 +0600

    stream op...

--- src/main/java/com/interview/streams/StreamOperation.java
+++ src/main/java/com/interview/streams/StreamOperation.java
@@ -1,6 +1,8 @@
 package com.interview.streams;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamOperation {
 
@@ -15,6 +17,17 @@ public class StreamOperation {
         System.out.println(sum);
 
         List<String> names = List.of("MD ASHIK ALI KHAN",
-                "LEO", "ASHIK", "");
+                "LEO", "ASHIK", "AKRAM", "ABRAR", "RAFIQ", "MD AKRAM");
+
+        List<String> stringList = names.stream().filter(name -> name.startsWith("A") || name.startsWith("M"))
+                .map(String::toLowerCase).collect(Collectors.toList());
+
+        System.out.println(stringList);
+
+        List<String> flatString = names.stream().flatMap(s -> Arrays.stream(s.split(" ")))
+                .collect(Collectors.toList());
+
+        System.out.println(flatString);
+
     }
 }
