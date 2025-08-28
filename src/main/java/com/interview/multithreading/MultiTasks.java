d75d00cdac53aeb7221437be3b35110f82d4015c
mdashikalikhan <khan.ashik@gmail.com>
2025-08-28 15:56:40 +0600

    Future vs CompleatableFuture

--- src/main/java/com/interview/multithreading/MultiTasks.java
+++ src/main/java/com/interview/multithreading/MultiTasks.java
@@ -1,16 +1,16 @@
 package com.interview.multithreading;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class MultiTasks {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        blockingExample();
+        blockingExample1();
+        blockingExample2();
+        nonBlockingExample1();
     }
 
-    static void blockingExample() throws ExecutionException, InterruptedException {
+    static void blockingExample1() throws ExecutionException, InterruptedException {
         ExecutorService executorService
                 = Executors.newFixedThreadPool(2);
 
@@ -24,11 +24,95 @@ public class MultiTasks {
 
         System.out.println("Result: " + result);
 
+
         executorService.shutdown();
     }
 
-    static void nonBlockingExample(){
+    static void blockingExample2() throws InterruptedException, ExecutionException {
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(100);
 
+        List<Callable<Integer>> lists = List.of(
+                ()->{
+                    Thread.sleep(2000);
+                    return 100;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 200;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 300;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 400;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 500;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 600;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 700;
+                },
+                ()->{
+                    Thread.sleep(2000);
+                    return 800;
+                }
+        );
+        List<Future<Integer>> futures = executorService.invokeAll(lists);
+
+        futures.stream().forEach(f-> {
+            try {
+                System.out.println(f.get());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+
+            }
+        });
+
+
+
+        executorService.shutdown();
+
+        executorService.shutdown();
+        System.out.println("All tasks complete");
+
+
+
+
+    }
+
+    static void nonBlockingExample1() throws ExecutionException, InterruptedException {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(
+                () -> {
+                    try {
+                        Thread.sleep(6000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return 100;
+                }
+        );
+
+        future.thenApply(r->r*100)
+                .thenAccept(System.out::println);
+
+
+
+
+        future.thenAccept(System.out::println);
+
+        System.out.println("Main Thread completes");
+
+
+        while(!future.isDone());
     }
 }
 
