240db2546d2e5c42cc3c483ccc5ca82fe910e6a1
mdashikalikhan <khan.ashik@gmail.com>
2025-11-06 13:00:18 +0600

    method reference

--- src/main/java/com/features/methodreference/MethodReferenceExample.java
+++ src/main/java/com/features/methodreference/MethodReferenceExample.java
@@ -4,16 +4,22 @@ import com.features.model.Student;
 import com.features.model.StudentDataBase;
 
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class MethodReferenceExample {
 
 
-    static Predicate<Student> p1 = (s) -> s.getGradeLevel()>=3;
+    static Predicate<Student> p1 = MethodReferenceExample::greaterThanGradeLvel;
 
-    /*public static boolean greaterThanGradeLvel(Student s){
-
-    }*/
+    public static boolean greaterThanGradeLvel(Student s){
+        return s.getGradeLevel()>3;
+    }
     public static void main(String[] args) {
-        p1.test(StudentDataBase.studentSupplier.get());
+        //p1.test(StudentDataBase.studentSupplier.get());
+        System.out.println(p1.test(StudentDataBase.studentSupplier.get()));
+
+        Supplier<Student> studentSupplier = Student::new;
+
+        System.out.println(studentSupplier.get());
     }
 }
