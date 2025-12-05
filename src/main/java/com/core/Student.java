99be58daf068d65e67d9fdc6f4009d8010033c66
MD ASHIK ALI KHAN <khan.ashik@gmail.com>
2025-12-05 12:07:37 +0600

    comparable..

--- src/main/java/com/core/Student.java
+++ src/main/java/com/core/Student.java
@@ -1,6 +1,11 @@
 package com.core;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Student implements Comparable<Student>{
 
     private int id;
@@ -13,8 +18,54 @@ public class Student implements Comparable<Student>{
         this.marks = marks;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
     @Override
     public int compareTo( Student o) {
         return Integer.compare(this.id, o.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(10, "A", 10));
+        students.add(new Student(11, "B", 20));
+        students.add(new Student(1, "C", 30));
+        students.add(new Student(8, "D", 5));
+        students.add(new Student(7, "B", 30));
+
+        Collections.sort(students);
+
+        System.out.println(students);
+
+        Comparator<Student> comparator = (s1, s2) -> s2.marks - s1.marks;
+
+        Collections.sort(students, comparator);
+
+        System.out.println(students);
+
+        students.sort(Comparator.comparing(Student::getMarks).reversed()
+        .thenComparing(Comparator.comparing(Student::getName)));
+
+        System.out.println(students);
+
     }
 }
